f7a8797f3f1fb8375df8d2c264e7df9ef33de2e7
nick <sonic7901@hotmail.com>
2022-07-19 17:23:02 +0800

    init spire.doc

---
+++ src/main/java/vas/Main.java
@@ -0,0 +1,19 @@
+package vas;
+
+import com.spire.doc.*;
+import com.spire.doc.documents.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Create word document.
+        Document document =new Document();
+        //Add a new section.
+        Section section = document.addSection();
+        //Add a new paragraph.
+        Paragraph paragraph = section.addParagraph();
+        //Append Text.
+        paragraph.appendText("Hello World!");
+        //Save to file.
+        document.saveToFile("output/helloWorld.docx", FileFormat.Docx);
+    }
+}
